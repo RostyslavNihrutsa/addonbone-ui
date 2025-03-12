e15add318748df3273547cb20a9a0b6ec861478a
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-03-13 01:52:06 +0200

    Installed react, storybook and some dependeciens

---
+++ .storybook/preview.tsx
@@ -0,0 +1,80 @@
+import React, {useEffect} from 'react';
+import type {Preview, StoryContext, StoryFn} from '@storybook/react'
+import '../src/styles/variables-default.css'
+import '../src/styles/preview.css'
+
+
+const themeDecorator = (Story: StoryFn, context: StoryContext) => {
+    const theme = context.globals.theme || 'light';
+    const cssVariables = context.globals.cssVariables || 'default';
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        return () => {
+            document.documentElement.removeAttribute('data-theme');
+        };
+    }, [theme]);
+
+    useEffect(() => {
+        if (cssVariables === 'default') return
+
+        const linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = '/src/styles/variables-custom.css';
+        document.head.appendChild(linkElement);
+
+        return () => {
+            document.head.removeChild(linkElement);
+        };
+    }, [cssVariables]);
+
+    return (
+        <div className='story-wrapper'>
+            {Story(context.args, context)}
+        </div>
+    )
+};
+
+const globalTypes = {
+    theme: {
+        name: 'Theme',
+        description: 'Global theme for components',
+        defaultValue: 'light',
+        toolbar: {
+            icon: 'star',
+            items: [
+                {value: 'light', title: 'Light theme'},
+                {value: 'dark', title: 'Dark theme'},
+            ],
+            showName: true,
+        },
+    },
+    cssVariables: {
+        name: 'CSS Variables',
+        description: 'Choose CSS variables set',
+        defaultValue: 'default',
+        toolbar: {
+            icon: 'paintbrush',
+            items: [
+                {value: 'default', title: 'Default Variables'},
+                {value: 'custom', title: 'Custom Variables'},
+            ],
+            showName: true,
+        },
+    },
+};
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+    decorators: [themeDecorator],
+    globalTypes
+};
+
+export default preview;
