b7559b87ac3db98980c16b79b2e8b0fc7e48d5b2
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-03-26 16:04:04 +0200

    Added new common Dialog component MBA-75

--- src/theme/context.ts
+++ src/theme/context.ts
@@ -4,6 +4,7 @@ import type {
     AvatarProps,
     ButtonProps,
     CheckboxProps,
+    DialogProps,
     DrawerProps,
     HeaderProps,
     IconButtonProps,
@@ -20,6 +21,7 @@ export interface DefaultProps {
     avatar?: Pick<AvatarProps, 'size' | 'radius' | 'cursorPointer' | 'delayMs'>;
     button?: Pick<ButtonProps, 'variant' | 'color' | 'size' | 'radius'>;
     checkbox?: Pick<CheckboxProps, 'variant' | 'size' | 'radius' | 'checkedIcon' | 'indeterminateIcon'>;
+    dialog?: DialogProps;
     drawer?: DrawerProps;
     header?: Pick<HeaderProps, 'alignCenter' | 'before' | 'after'>;
     iconButton?: Pick<IconButtonProps, 'variant' | 'size' | 'radius'>;
