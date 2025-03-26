a6138e884c3e65bc6d5389487a5fc59748820340
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-03-26 16:23:51 +0200

    Applied Dialog in Drawer component MBA-75

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,20 +1,10 @@
 import React, {FC, memo} from "react";
 import classnames from "classnames";
-import {
-    Content,
-    Description,
-    DialogContentProps,
-    DialogPortalProps,
-    DialogProps as DialogRootProps,
-    Overlay,
-    Portal,
-    Root,
-    Title
-} from '@radix-ui/react-dialog';
-import {VisuallyHidden} from "radix-ui";
 
 import {useDefaultProps} from "../../theme";
 import {cloneOrCreateElement} from "../../utils";
+
+import {Dialog, DialogProps} from "../Dialog"
 
 import styles from "./drawer.module.scss"
 
@@ -25,29 +15,17 @@ export enum DrawerSide {
     Bottom = "bottom",
 }
 
-export interface DrawerProps extends DialogRootProps, DialogPortalProps, DialogContentProps {
+export interface DrawerProps extends DialogProps {
     side?: DrawerSide;
-    description?: string;
-    fullscreen?: boolean;
-    className?: string;
-    overlayClassName?: string;
-    childrenClassName?: string;
 }
 
 const Drawer: FC<DrawerProps> = (props) => {
     const defaultProps = useDefaultProps('drawer');
     const mergedProps = {...defaultProps, ...props};
     const {
-        open,
-        defaultOpen,
-        onOpenChange,
-        modal,
-        children,
-        container,
         side = DrawerSide.Left,
-        title,
-        description,
         fullscreen,
+        children,
         className,
         overlayClassName,
         childrenClassName,
@@ -55,22 +33,17 @@ const Drawer: FC<DrawerProps> = (props) => {
     } = mergedProps;
 
     return (
-        <Root open={open} defaultOpen={defaultOpen} onOpenChange={onOpenChange} modal={modal}>
-            <Portal container={container}>
-                <Overlay className={classnames(styles["drawer-overlay"], overlayClassName)}/>
-                <Content className={classnames(styles["drawer-content"], {
-                    [styles["drawer-content--fullscreen"]]: fullscreen,
-                    [styles[`drawer-content--${side}-side`]]: side,
-                }, className)} {...other}>
-                    <VisuallyHidden.Root>
-                        <Title>{title}</Title>
-                        <Description>{description}</Description>
-                    </VisuallyHidden.Root>
-                    {cloneOrCreateElement(children, {className: classnames(styles["drawer-children"], childrenClassName)}, 'div')}
-                </Content>
-            </Portal>
-        </Root>
-    );
+        <Dialog
+            {...other}
+            overlayClassName={classnames(styles["drawer-overlay"], overlayClassName)}
+            className={classnames(styles["drawer-content"], {
+                [styles["drawer-content--fullscreen"]]: fullscreen,
+                [styles[`drawer-content--${side}-side`]]: side,
+            }, className)}
+        >
+            {cloneOrCreateElement(children, {className: classnames(styles["drawer-children"], childrenClassName)}, 'div')}
+        </Dialog>
+    )
 };
 
 export default memo(Drawer);
