ceb972764d526e01867f33f6ca2f02d9c9a45c34
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-07-14 19:14:37 +0300

    Enhance theme handling in ThemeProvider

--- src/providers/theme/ThemeProvider.tsx
+++ src/providers/theme/ThemeProvider.tsx
@@ -1,24 +1,37 @@
-import React, {FC, PropsWithChildren, useCallback, useEffect, useState} from "react";
+import React, {FC, PropsWithChildren, useCallback, useEffect, useRef, useState} from "react";
+
+import {Storage} from 'adnbn/storage';
 
 import {ThemeContext} from "./context";
 
-import {Theme} from "../../types/theme";
+import {Theme, themeKey} from "../../types/theme";
 import {Config} from "../../types/config";
 
 const isDarkMedia = () => window?.matchMedia("(prefers-color-scheme: dark)")?.matches;
 
 const ThemeProvider: FC<PropsWithChildren<Pick<Config, "components">>> = ({children, components}) => {
+    const storage = useRef<Storage<Record<string, Theme>>>(new Storage());
+
     const [theme, setTheme] = useState<Theme>(() => {
         return isDarkMedia() ? Theme.Dark : Theme.Light;
     });
 
     const changeTheme = useCallback((theme: Theme) => {
         setTheme(theme);
+
+        storage.current.set(themeKey, theme)
+            .catch(e => console.error('ThemeProvider, set theme to storage error', e));
     }, []);
 
     const toggleTheme = useCallback(() => {
         changeTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
     }, [theme, changeTheme]);
+
+    useEffect(() => {
+        storage.current.get(themeKey)
+            .then(theme => theme && setTheme(theme))
+            .catch(e => console.error('ThemeProvider, get theme from storage error', e));
+    }, []);
 
     useEffect(() => {
         document.querySelector("html")?.setAttribute("theme", theme);
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -2,3 +2,5 @@ export enum Theme {
     Light = "light",
     Dark = "dark",
 }
+
+export const themeKey = "theme";
