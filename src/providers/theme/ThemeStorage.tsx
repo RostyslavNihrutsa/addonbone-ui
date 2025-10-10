6f68bde44b326dfa1b0a44ae94706b011b94902c
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-10-10 17:34:59 +0300

    feat(styles): add new scss mixins, add `view` prop to UIProvider

--- src/providers/theme/ThemeStorage.tsx
+++ src/providers/theme/ThemeStorage.tsx
@@ -5,7 +5,7 @@ import {Theme, ThemeStorageContract} from "../../types/theme";
 export default class implements ThemeStorageContract {
     private readonly storage = new Storage<Record<string, Theme>>({
         area: "local",
-        namespace: "adnbn-ui",
+        namespace: "addon-ui",
     });
 
     private readonly key = "theme";
